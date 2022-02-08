18f897489eb546b4a6e0d912a4946a013836f0ba
almasud <almasud.dev@gmail.com>
2022-02-08 10:36:01 +0600

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Abdullah Almasud on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
